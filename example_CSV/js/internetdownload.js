50a924009194cd394132ee970f991b698f1f8f7b
RoyCai <iou04121208@gmail.com>
2016-01-17 21:17:54 +0800

    fix some problem

--- example_CSV/js/internetdownload.js
+++ example_CSV/js/internetdownload.js
@@ -1,79 +1,128 @@
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
 
-function JSONToCSVConvertor(JSONData,ShowLabel) {    
-	var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;   
-	var CSV = '';     
-	if (ShowLabel) {
-		 var row = "";
-		 for (var index in arrData[0]) {
-				 row += index + ',';
-		 }
-		 row = row.slice(0, -1);
-		 CSV += row + '\r\n';
-	}
-	for (var i = 0; i < arrData.length; i++) {
-		 var row = "";
-		 for (var index in arrData[i]) {
-				var arrValue = arrData[i][index] == null ? "" : '="' + arrData[i][index] + '"';
-				row += arrValue + ',';
-		 }
-		 row.slice(0, row.length - 1);
-		 CSV += row + '\r\n';
-	}
-	if (CSV == '') {        
-		 growl.error("Invalid data");
-		 return;
-	}   
-	var fileName = "Result";
-	if(msieversion()){
-	var IEwindow = window.open();
-	IEwindow.document.write('sep=,\r\n' + CSV);
-	IEwindow.document.close();
-	IEwindow.document.execCommand('SaveAs', true, fileName + ".csv");
-	IEwindow.close();
-	} else {
-	 var uri = 'data:application/csv;charset=utf-8,' + escape(CSV);
-	 var link = document.createElement("a");    
-	 link.href = uri;
-	 link.style = "visibility:hidden";
-	 link.download = fileName + ".csv";
-	 document.body.appendChild(link);
-	 link.click();
-	 document.body.removeChild(link);
-	}
+    var CSV = '';
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+
+        row = row.slice(0, -1);
+
+        //append Label row with line break
+        CSV += row + '\n';
+    }
+
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {
+        alert("Invalid data");
+        return;
+    }
+
+    //Generate a file name
+    var fileName = "MyReport_";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g, "_");
+
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
-function msieversion() {
-	var ua = window.navigator.userAgent; 
-	var msie = ua.indexOf("MSIE "); 
-	if (msie != -1 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number 
-	{
-		return true;
-	} else { // If another browser, 
-		return false;
-	}
-		return false; 
-}
 
-function csvJSON(csv){
+function csvJSON(csv) {
 
-  var lines=csv.split("\n");
+    var lines = csv.split("\r\n");
+    var result = [];
+    var headers = lines[0].split(",");
 
-  var result = [];
+    for (var i = 1; i < lines.length; i++) {
+        var obj = {};
 
-  var headers=lines[0].split(",");
+        var row = lines[i],
+            queryIdx = 0,
+            startValueIdx = 0,
+            idx = 0;
 
-  for(var i=1;i<lines.length;i++){
+        if (row.trim() === '') {
+            continue;
+        }
 
-	  var obj = {};
-	  var currentline=lines[i].split(",");
+        while (idx < row.length) {
+            /* if we meet a double quote we skip until the next one */
+            var c = row[idx];
 
-	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
-	  }
+            if (c === '"') {
+                do {
+                    c = row[++idx];
+                } while (c !== '"' && idx < row.length - 1);
+            }
 
-	  result.push(obj);
+            if (c === ',' || /* handle end of line with no comma */ idx === row.length - 1) {
+                /* we've got a value */
+                var value = row.substr(startValueIdx, idx - startValueIdx).trim();
 
-  }
-  
-  //return result; //JavaScript object
-  return JSON.stringify(result); //JSON
+                /* skip first double quote */
+                if (value[0] === '"') {
+                    value = value.substr(1);
+                }
+                /* skip last comma */
+                if (value[value.length - 1] === ',') {
+                    value = value.substr(0, value.length - 1);
+                }
+                /* skip last double quote */
+                if (value[value.length - 1] === '"') {
+                    value = value.substr(0, value.length - 1);
+                }
+
+                var key = headers[queryIdx++];
+                obj[key] = value;
+                startValueIdx = idx + 1;
+            }
+
+            ++idx;
+        }
+
+        result.push(obj);
+    }
+
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
 }
